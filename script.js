9d714289ea5f8c56875adf0e42cece410430ca79
prazzon <prazzon2@gmail.com>
2023-02-01 16:31:58 +0100

    feat: add multiple select option for flex-items * add the ability to select and deselect a flex-item on click * add the ability to deselect all selected flex-items when an empty playground target is clicked
    style: update display flex toggle and favicon
    refactor: refactor script.js for clarity
    
    This commit includes updates to both features and styles in the project. The multiple select option for flex-items has been added, along with the ability to select and deselect a flex-item on click and the ability to deselect all selected flex-items when an empty playground target is clicked. The display flex toggle and favicon have been updated. The script.js file has also been refactored to improve clarity and make it easier for others to understand.

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const alignContentBtns = document.getElementById("align-content-btns");
 
 const flexItemContainer = document.getElementById('flex-item-container');
 const closeFlexItemBtn = document.getElementById('close-flex-item-btn');
+const optionContainer = document.getElementById("option");
+const selectMultipleCheckbox = document.getElementById('select-multiple-checkbox');
 const orderInput = document.getElementById("order-input");
 const flexGrowInput = document.getElementById("flex-grow-input");
 const flexShrinkInput = document.getElementById("flex-shrink-input");
@@ -29,30 +31,18 @@ const cssCode = document.getElementById("css-code");
 const copyCodeBtn = document.getElementById("copy-code-btn");
 const alert = document.getElementById("alert");
 
-
-// Create a variable to store the current playground item index
-let currentPlaygroundItem;
+// Create an array to store the selected playground item index
+let selectedPlaygroundItems = [];
 
 // Create 3 playground items on page load
 for (let i = 0; i < 3; i++) {
-    // createPlaygroundItem(i);
-
     // Add playground item after .5 second of each item
     setTimeout(() => {
         createPlaygroundItem(i);
     }, 500 * i);
 }
 
-// Add click event to add item button
-addItemBtn.addEventListener('click', () => {
-    // Get the last playground item index
-    const lastPlaygroundItem = playground.children.length - 1;
-
-    // Create a new playground item
-    createPlaygroundItem(lastPlaygroundItem + 1);
-});
-
-// Add click event to display toggle buttons and all flex container style buttons
+// Add click event to each flex container style elements
 addClickEvent(displayToggle, 'display', 'playground-flex');
 addClickEvent(flexDirectionBtns, 'flexDirection', 'playground-btn');
 addClickEvent(flexWrapBtns, 'flexWrap', 'playground-btn');
@@ -60,7 +50,7 @@ addClickEvent(justifyContentBtns, 'justifyContent', 'playground-btn');
 addClickEvent(alignItemsBtns, 'alignItems', 'playground-btn');
 addClickEvent(alignContentBtns, 'alignContent', 'playground-btn');
 
-// Add event listeners to each flex item elements
+// Add event listeners to each flex item style elements
 addInputEvent(orderInput, 'order');
 addInputEvent(flexGrowInput, 'flexGrow');
 addInputEvent(flexShrinkInput, 'flexShrink');
@@ -72,28 +62,58 @@ addInputEvent(ItemWidthInput, 'minWidth');
 addInputEvent(ItemHeightInput, 'minHeight');
 addCheckboxEvent(centerTextCheckbox, 'checked');
 
-// Event listener to close flex item container
-closeFlexItemBtn.addEventListener('click', () => {
-    flexItemContainer.classList.remove("active");
+// Event listener for the add item button
+addItemBtn.addEventListener('click', () => {
+    // Get the last playground item index
+    const lastPlaygroundItem = playground.children.length;
 
-    // Remove all active class on the playground item
-    const playgroundItems = document.querySelectorAll(".playground-item");
-    playgroundItems.forEach((item) => {
-        item.firstElementChild.classList.remove("active"); 
-        item.classList.remove("active");
-    });
+    // Create a new playground item
+    createPlaygroundItem(lastPlaygroundItem);
 });
 
-// Event listener to delete the current playground item
+// Event listener to close flex item container when playground is clicked
+playground.addEventListener('click', (e) => {
+    if (e.target.classList.contains("playground")) {
+        closeFlexItemContainer();
+    }
+});
+
+// Event listener for the close button
+closeFlexItemBtn.addEventListener('click', closeFlexItemContainer);
+
+// Event listener for the delete playground item button
 deleteItemBtn.addEventListener('click', () => {
-    // Remove the current playground item
+    // Remove active class on the flex item container
     flexItemContainer.classList.remove("active");
-    playground.children[currentPlaygroundItem].classList.remove("active")
-    playground.children[currentPlaygroundItem].classList.add('remove');
-    // playground.children[currentPlaygroundItem].remove();
-    setTimeout(() => {
-        playground.children[currentPlaygroundItem].remove();
-    }, 390);
+
+    // Sort the selected playground item array in descending order to prevent index error
+    selectedPlaygroundItems.sort((a, b) => b - a);
+    
+    // Remove all playground item in the selected playground item array
+    selectedPlaygroundItems.forEach((item) => {
+        // Trigger remove animation
+        playground.children[item].classList.remove("active");
+        playground.children[item].classList.add("remove");
+
+        // Remove playground item after 390ms
+        setTimeout(() => {
+            playground.children[item].remove();
+        }, 390);
+    });
+        // Clear the selected playground item array
+        selectedPlaygroundItems = [];
+});
+
+// Event listener for the select multiple checkbox
+selectMultipleCheckbox.addEventListener('change', (e) => {
+    // Check if the select multiple checkbox is checked
+    if (e.target.checked) {
+        // Add active class to the option container
+        optionContainer.classList.add("active");
+    } else {
+        // Remove active class to the option container
+        optionContainer.classList.remove("active");
+    }
 });
 
 // Event listener to open and close the modal
@@ -175,17 +195,18 @@ copyCodeBtn.addEventListener('click', () => {
 function createPlaygroundItem(index) {
     // Create a new playground item
     const playgroundItem = document.createElement('div');
+    const playgroundItemText = document.createElement('div');
     playgroundItem.classList.add('playground-item');
     playgroundItem.classList.add('show');
-    setTimeout(() => {
-        playgroundItem.classList.remove('show');
-    }, 1000);
-    const playgroundItemText = document.createElement('div');
     playgroundItemText.classList.add('playground-item-text');
     playgroundItemText.textContent = index + 1;
     playgroundItem.appendChild(playgroundItemText);
-    // playgroundItem.textContent = 'Item ' + (index + 1);
-    
+
+    // Remove the show class after 1 second to prevent animation when element selection is changed
+    setTimeout(() => {
+        playgroundItem.classList.remove('show');
+    }, 1000);
+
     // Set default style properties for the new playground item
     playgroundItem.style.order = 0;
     playgroundItem.style.flexGrow = 0;
@@ -199,54 +220,111 @@ function createPlaygroundItem(index) {
     
     // Add click event to playground item
     playgroundItem.addEventListener('click', (e) => {
-        // Add active class to flex-item-container
-        flexItemContainer.classList.add("active");
-
-        // Remove all active class on the playground item
-        const playgroundItems = document.querySelectorAll('.playground-item');
-        playgroundItems.forEach((item) => {
-            item.classList.remove("active");
-            item.firstElementChild.classList.remove("active");
-        })
-        
-        // Get the clicked element's index and update the currentPlaygroundItem variable
-        currentPlaygroundItem = Array.from(playground.children).indexOf(e.target);
+        // Get the clicked element's index
+        let currentPlaygroundItem = Array.from(playground.children).indexOf(e.target);
 
         // Fix the currentPlaygroundItem variable if the clicked element is the playground item's text
         if (currentPlaygroundItem === -1) {
             currentPlaygroundItem = Array.from(playground.children).indexOf(e.target.parentElement);
         }
 
-        // Add active class to the current playground element
-        playground.children[currentPlaygroundItem].firstElementChild.classList.add("active");
-        playground.children[currentPlaygroundItem].classList.add("active");
+        // Add the current element index to the selected playground item if it doesn't exist in the array
+        if (!selectedPlaygroundItems.includes(currentPlaygroundItem)) {
+            // Check if the selectMultipleCheckbox is not checked
+            if (!selectMultipleCheckbox.checked) {
+                // Remove the active class from all playground items
+                const playgroundItems = document.querySelectorAll('.playground-item');
+                playgroundItems.forEach((item) => {
+                    item.classList.remove("active");
+                    item.firstElementChild.classList.remove("active");
+                });
 
-        // Get the clicked element's style properties and update the input values
-        orderInput.value = playground.children[currentPlaygroundItem].style.order;
-        flexGrowInput.value = playground.children[currentPlaygroundItem].style.flexGrow;
-        flexShrinkInput.value = playground.children[currentPlaygroundItem].style.flexShrink;
-        flexBasisInput.value = playground.children[currentPlaygroundItem].style.flexBasis;
-        itemTextInput.value = playground.children[currentPlaygroundItem].textContent;
-        fontSizeInput.value = playground.children[currentPlaygroundItem].dataset.fontSize;
-        ItemWidthInput.value = playground.children[currentPlaygroundItem].dataset.minWidth;
-        ItemHeightInput.value = playground.children[currentPlaygroundItem].dataset.minHeight;
-        centerTextCheckbox.checked = playground.children[currentPlaygroundItem].dataset.centerText === 'true' ? true : false;
+                // Empty the selected playground items array
+                selectedPlaygroundItems = [];
+            }
+            
+            selectedPlaygroundItems.push(currentPlaygroundItem);
+        } else {
+            // Remove the current element index from the selected playground item if it exists in the array
+            selectedPlaygroundItems = selectedPlaygroundItems.filter((item) => item !== currentPlaygroundItem);
+        }
 
-        // Add active class to the current element align-self property
-        for (btnEl of alignSelfBtns.children) {
-            if (btnEl.textContent.toLowerCase() === playground.children[currentPlaygroundItem].style.alignSelf) {
-                btnEl.classList.add("active");
-            } else {
-                btnEl.classList.remove("active");
+        // Toggle the active class on the playground item
+        playground.children[currentPlaygroundItem].classList.toggle("active");
+        playground.children[currentPlaygroundItem].firstElementChild.classList.toggle("active");
+
+        // Add active class to flex-item-container if there is at least one selected playground item
+        if (selectedPlaygroundItems.length > 0) {
+            flexItemContainer.classList.add("active");
+        } else {
+            flexItemContainer.classList.remove("active");
+        }
+
+        // Change flex-item-container's container title text if there is more than one selected playground item
+        const flexItemContainerTitle = document.querySelector('.flex-item-container > .container-title');
+        if (selectedPlaygroundItems.length > 1) {
+            flexItemContainerTitle.textContent = 'Flex Item(' + selectedPlaygroundItems.length + ')';
+        } else {
+            flexItemContainerTitle.textContent = 'Flex Item';
+        }
+
+        // Get the last element from the selected playground items array and update the input values
+        if (selectedPlaygroundItems.length > 0) {
+            const lastSelectedItem = selectedPlaygroundItems[selectedPlaygroundItems.length - 1];
+            orderInput.value = playground.children[lastSelectedItem].style.order;
+            flexGrowInput.value = playground.children[lastSelectedItem].style.flexGrow;
+            flexShrinkInput.value = playground.children[lastSelectedItem].style.flexShrink;
+            flexBasisInput.value = playground.children[lastSelectedItem].style.flexBasis;
+            itemTextInput.value = playground.children[lastSelectedItem].textContent;
+            fontSizeInput.value = playground.children[lastSelectedItem].dataset.fontSize;
+            ItemWidthInput.value = playground.children[lastSelectedItem].dataset.minWidth;
+            ItemHeightInput.value = playground.children[lastSelectedItem].dataset.minHeight;
+            centerTextCheckbox.checked = playground.children[lastSelectedItem].dataset.centerText === 'true' ? true : false;
+            
+            // Add active class to the current element align-self property
+            for (btnEl of alignSelfBtns.children) {
+                if (
+                    btnEl.textContent.toLowerCase() ===
+                    playground.children[currentPlaygroundItem].style.alignSelf
+                ) {
+                    btnEl.classList.add("active");
+                } else {
+                    btnEl.classList.remove("active");
+                }
             }
         }
-    })
 
+        // If playground item text is clicked, set the itemTextInput to focus state
+        if (e.target.classList.contains('playground-item-text')) {
+            // Check if flex item container is active
+            if (flexItemContainer.classList.contains('active')) {
+                // Set the itemTextInput to focus state and select the text
+                itemTextInput.focus({ preventScroll: true });
+                itemTextInput.select();
+            }
+        }
+    });
+    
     // Append the new playground item to the playground
     playground.appendChild(playgroundItem);
 }
 
-// Function to add click events to all flex container style buttons
+// Function to close flex item container
+function closeFlexItemContainer() {
+    flexItemContainer.classList.remove("active");
+
+    // Remove all active class on the playground item
+    const playgroundItems = document.querySelectorAll(".playground-item");
+    playgroundItems.forEach((item) => {
+        item.firstElementChild.classList.remove("active");
+        item.classList.remove("active");
+    });        
+
+    // Clear the selected playground item array
+    selectedPlaygroundItems = [];
+}
+
+// Function to add click events to buttons
 function addClickEvent(parentEl, styleProp, type) {
     for ( el of parentEl.children ) {
         el.addEventListener('click', (e) => {
@@ -271,45 +349,53 @@ function addClickEvent(parentEl, styleProp, type) {
                     parentEl.classList.add("active");
                 }
 
-                // Remove inline and active classes if inline-flex
+                // Remove inline and active classes if flex
                 if (e.target.textContent.toLowerCase() === "flex") {
                     playground.classList.remove("inline");
                     parentEl.classList.remove("active");
                 }
             } else {
-                // Change style property of current element
-                playground.children[currentPlaygroundItem].style.alignSelf =
-                    e.target.textContent.toLowerCase();
+                // Change style property of each element in the selected playground items array
+                selectedPlaygroundItems.forEach(element => {
+                    playground.children[element].style[styleProp] = e.target.textContent.toLowerCase();
+                });
             }
-        })
+        });
     }
 }
 
-// Function to add input event to flex-item input element
+// Function to add input event to flex-item inputs
 function addInputEvent(inputEl, styleProp, type) {
     inputEl.addEventListener('input', (e) => {
-        // Change the clicked element's text content to the clicked element's value
-        if (styleProp === 'textContent') {
-            playground.children[currentPlaygroundItem].firstElementChild.textContent = e.target.value;
-        } else if (styleProp === 'fontSize' || styleProp === 'minWidth' || styleProp === 'minHeight') {
-            // Change the clicked element's font size to the clicked element's value
-            playground.children[currentPlaygroundItem].style[styleProp] = e.target.value + 'px';
-            playground.children[currentPlaygroundItem].dataset[styleProp] = e.target.value;
-        } else {
-            // Change the clicked element's style property to the clicked element's value
-            playground.children[currentPlaygroundItem].style[styleProp] = e.target.value;
-        }
+        // Change style properties of each element in the selected playground items array
+        selectedPlaygroundItems.forEach(element => {
+            // Change the element text content to the clicked element's value
+            if (styleProp === 'textContent') {
+                playground.children[element].firstElementChild.textContent = e.target.value;
+            } else if (styleProp === 'fontSize' || styleProp === 'minWidth' || styleProp === 'minHeight') {
+                // Change the element font size to the clicked element's value
+                playground.children[element].style[styleProp] = e.target.value + 'px';
+                playground.children[element].dataset[styleProp] = e.target.value;
+            } else {
+                // Change the element style property to the clicked element's value
+                playground.children[element].style[styleProp] = e.target.value;
+            }
+        });
     })
 }
 
-// Function to add checkbox event to flex-item checkbox element
+// Function to add checkbox event to flex-item checkbox
 function addCheckboxEvent(checkboxEl, toggleClass) {
     checkboxEl.addEventListener('change', (e) => {
-        // Toggle checked class
-        playground.children[currentPlaygroundItem].firstElementChild.classList.toggle(toggleClass);
-
-        // Update centerText data attribute
-        playground.children[currentPlaygroundItem].dataset.centerText = e.target.checked;
+        // Toggle checked class and update centerText data attribute of each element in the selected playground items array
+        selectedPlaygroundItems.forEach(element => {
+            if (checkboxEl.checked) {
+                playground.children[element].firstElementChild.classList.add(toggleClass);
+            } else {
+                playground.children[element].firstElementChild.classList.remove(toggleClass);
+            }
+            playground.children[element].dataset.centerText = e.target.checked;
+        });
     })
 }
 
